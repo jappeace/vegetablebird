885407b3027462882dd773af513a85b27c9aaa2d
Jappie Klooster <jappieklooster@hotmail.com>
2015-12-05 14:48:40 +0100

    ScalaJS for the future!

---
+++ build.sbt
@@ -0,0 +1,4 @@
+enablePlugins(ScalaJSPlugin)
+name := "Vegetable Bird"
+scalaVersion := "2.11.7"
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
---
+++ src/main/scala/nl/jappieklooster/vegetablebird/Main.scala
@@ -0,0 +1,8 @@
+package nl.jappieklooster.vegetablebird
+
+import scala.scalajs.js.JSApp
+object Main extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
